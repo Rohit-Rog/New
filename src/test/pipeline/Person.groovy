13d3800dbbf2073f9e021277bab71d6a795708b4
Rohit Sarkar <rohitpoets13@gmail.com>
2020-03-08 13:55:15 +0530

    src changed

---
+++ src/test/pipeline/Person.groovy
@@ -0,0 +1,23 @@
+package test.pipeline
+
+class Person{
+	int age
+	String name
+
+	Person(String name, int age){
+		this.name=name
+		this.age=age
+	}
+	
+	String getName(){
+		return this.Name
+	}
+	
+	int getAge(){
+		return this.age
+	}
+	
+	void details(){
+		println "${this.name}'s age is ${this.age}"
+	}
+}
